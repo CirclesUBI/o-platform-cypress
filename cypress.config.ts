e641c441911856392c6c624c190d5ee1da29b587
jaensen <4954577+jaensen@users.noreply.github.com>
2023-02-28 22:41:12 +0100

    larger margin around the qr code

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,14 +5,14 @@ export default defineConfig({
     "*.localhost": "127.0.0.1"
   },
   e2e: {
-    supportFile: "support/initVideo.ts",
+    // supportFile: "support/initVideo.ts",
     baseUrl: "https://o-platform.localhost",
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser:any = {}, launchOptions) => {
         if (browser.family === 'chromium' && browser.name !== 'electron') {
           // Mac/Linux
           launchOptions.args.push(
-              '--use-file-for-fake-video-capture=/tmp/Person_1.y4m'
+              '--use-file-for-fake-video-capture=http://localhost:1234/Person_1.y4m'
           )
         }
 
