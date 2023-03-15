b28fa4d9ee4043458012b18bb1a4e7bede952cfd
Ingo Collatz <incollatz@mail.de>
2023-03-15 14:51:27 +0200

    dockerize

--- support/initVideo.ts
+++ support/initVideo.ts
@@ -1,7 +1,7 @@
 function downloadCameraStreamVideo() {
     fetch('http://localhost:1234/Person_1.y4m').then(res => res.arrayBuffer()).then(buffer => {
         const fs = require('fs');
-        fs.writeFileSync('/tmp/Person_1.y4m', buffer);
+        fs.writeFileSync('./../cypress/fixtures/Person_1.y4m', buffer);
     });
 }
 
