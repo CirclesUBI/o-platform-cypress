a3775bc62d431e2c62d1ae30eb3431ab711c0589
Ingo Collatz <incollatz@mail.de>
2023-03-15 14:51:02 +0200

    dockerize

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,13 +6,13 @@ export default defineConfig({
   },
   e2e: {
     // supportFile: "support/initVideo.ts",
-    baseUrl: "https://o-platform.localhost",
+    baseUrl: "https://o-platform.circlesubi.localhost",
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser:any = {}, launchOptions) => {
         if (browser.family === 'chromium' && browser.name !== 'electron') {
           // Mac/Linux
           launchOptions.args.push(
-              '--use-file-for-fake-video-capture=http://localhost:1234/Person_1.y4m'
+              '--use-file-for-fake-video-capture=cypress/fixtures/Person_1.y4m'
           )
         }
 
