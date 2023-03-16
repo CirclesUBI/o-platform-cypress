d706cd21c84cd06eacf3653510d1c7145645a0e9
Ingo Collatz <incollatz@mail.de>
2023-03-17 00:29:37 +0200

    --use-fake-device-for-media-stream

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'cypress'
 
+console.log("hello from config");
+
 export default defineConfig({
   hosts: {
     "*.localhost": "127.0.0.1"
@@ -9,10 +11,11 @@ export default defineConfig({
     baseUrl: "https://o-platform.circlesubi.localhost",
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser:any = {}, launchOptions) => {
+        console.log("before:browser:launch - browser.family", browser.family)
         if (browser.family === 'chromium' && browser.name !== 'electron') {
           // Mac/Linux
           launchOptions.args.push(
-              '--use-fake-device-for-media-stream --use-file-for-fake-video-capture=/fixtures/Person_1.y4m'
+              '--use-fake-device-for-media-stream --use-file-for-fake-video-capture=/o-platform-cypress/cypress/fixtures/Person_1.y4m'
           )
         }
 
