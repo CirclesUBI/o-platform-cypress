b6f57411c0597a55f0e110f8d29bfdb9e8a4a5e2
jaensen <4954577+jaensen@users.noreply.github.com>
2023-03-18 13:55:40 +0100

    use __dirname to determine the path of the invite qr-code video fixture

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'cypress'
 
+const videoInput = __dirname + '/cypress/fixtures/Person_1.y4m';
+
 export default defineConfig({
   hosts: {
     "*.localhost": "127.0.0.1"
@@ -8,9 +10,12 @@ export default defineConfig({
     baseUrl: "https://o-platform.circlesubi.localhost",
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser:any = {}, launchOptions) => {
+        console.log('before:browser:launch', browser, launchOptions);
         if (browser.family === 'chromium' && browser.name !== 'electron') {
+          // auto open devtools
+          launchOptions.args.push('--auto-open-devtools-for-tabs')
           launchOptions.args.push(
-              '--use-file-for-fake-video-capture=/o-platform-cypress/cypress/fixtures/Person_1.y4m'
+              `--use-file-for-fake-video-capture=${videoInput}`
           )
         }
 
