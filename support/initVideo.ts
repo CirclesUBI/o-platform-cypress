e3a05b1fc6ebb0fd8368e2fb0b14cccd871c78d7
jaensen <4954577+jaensen@users.noreply.github.com>
2023-03-18 12:53:05 +0100

    use the cypress tests from o-platform-cypress via volume instead of cloning the repository into the container

--- support/initVideo.ts
+++
@@ -1,8 +0,0 @@
-function downloadCameraStreamVideo() {
-    fetch('http://localhost:1234/Person_1.y4m').then(res => res.arrayBuffer()).then(buffer => {
-        const fs = require('fs');
-        fs.writeFileSync('./../cypress/fixtures/Person_1.y4m', buffer);
-    });
-}
-
-downloadCameraStreamVideo();
