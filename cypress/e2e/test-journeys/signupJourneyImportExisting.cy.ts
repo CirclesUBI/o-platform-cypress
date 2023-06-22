7ad93737fd9eedd4daaa55710e868d4ff665b41f
Ingo Collatz <incollatz@mail.de>
2023-06-22 13:53:42 +0200

    separated survey-test

--- cypress/e2e/test-journeys/signupJourneyImportExisting.cy.ts
+++ cypress/e2e/test-journeys/signupJourneyImportExisting.cy.ts
@@ -1,35 +1,35 @@
-import { signupJourney } from "../modules/signupJourney";
+import { survey } from "../modules/survey";
 
-describe("tests for the sign-up journey", { testIsolation: false }, () => {
+describe("tests for the signup-survey", () => {
+
   it("clears all cookies, sessionStorage and localStorage", () => {
     cy.clearAllSessionStorage();
     cy.clearAllCookies();
     cy.clearAllLocalStorage();
-    cy.visit("/").then(($url) => { console.log($url) });
+    cy.visit("/");
   });
-  describe("journey", () => {
-    it("should pass the signup-journey", () => {
-      signupJourney();
+
+  it("runs tests for the survey", () => {
+    cy.visit("/#/passport/actions/logout");
+
+    cy.wait(5000);
+
+    cy.visit("/#/passport/actions/login/3");
+
+    cy.on("uncaught:exception", (err, runnable) => {
+      return false;
     });
 
-    it("imports existing account", () => {
-      cy.getByI18nKey("shared.molecules.nextNav.components.loginPill.signInNow")
-        .should("exist")
-        .click();
+    cy.get("input").each(($input) => {
+      cy.wrap($input).type("1");
+    });
 
-      cy.get("button").eq(1).should("exist").click();
+    cy.get("button[type=submit]").should("exist").click();
 
-      cy.get("textarea").type(Cypress.env("keyPhrase"));
+    cy.wait(1000);
 
-      cy.get("button[type=submit]").should("exist").click();
+    survey();
+  })
+})
 
-      cy.get("input").should("exist").click();
 
-      cy.contains("Person_9").should("exist").click();
-
-      cy.get("button[type=submit]").should("exist").click();
-      cy.get("button[type=submit]").should("exist").click();
-
-    })
-  });
-});
